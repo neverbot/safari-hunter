9ab22db96ad55f49253325c105ad7a83c96a3f33
Iván Alonso <ivan@neverbot.com>
2019-10-23 19:15:16 +0200

    update all code to work with experimental modules

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node --experimental-modules
 
-// # Application Bootup
+// Application Bootup
 // boots up the application
 
-// defines that javascript should be executed in 'strict mode' (ECMAScript 5)
-'use strict';
-
-// # Initialise CLI
+// Initialise CLI
 // imports the Command Line Interface (CLI) in order to make the application work
-module.exports = require('./lib/cli/index.js');
+import { main } from './lib/cli/index.js';
 
-// # Closing Application
+main();
+
+// Closing Application
 // make sure the manager gets stopped
 process.on('SIGINT', function() {
   process.exit();
